014dc65b648f99845e21423a6283b52e02ad3317
Dmitry Rocha <dmitryrck@gmail.com>
2016-01-18 07:24:13 -0300

    Add split_payment/create_authorization

---
+++ examples/split_payment/create_authorization.rb
@@ -0,0 +1,45 @@
+require_relative "../boot"
+
+# Create Authorization
+#
+#   You need to give:
+#     - notification_url
+#     - redirect_url
+#     - permissions defaults to all permissions
+#     - applications credentials (APP_ID, APP_KEY)
+#
+#   You can pass this parameters to PagSeguro::AuthorizationRequest#new
+#
+# PS: For more details look the class PagSeguro::AuthorizationRequest
+
+credentials = PagSeguro::ApplicationCredentials.new(
+  'xxxxxxxxxxxxx',
+  'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+)
+
+permissions = [
+  :checkouts,
+  :notifications,
+  :searches,
+  :pre_approvals,
+  :payments,
+  :refunds,
+  :cancels
+]
+
+options = {
+  credentials: credentials, # Unnecessary if you set in application config
+  permissions: permissions,
+  notification_url: 'http://example.com/',
+  redirect_url: 'http://example.com/',
+  reference: 'REF4321' # optional
+}
+
+authorization_request = PagSeguro::AuthorizationRequest.new(options)
+
+if authorization_request.create
+  print "Use this link to confirm authorizations: "
+  puts authorization_request.url
+else
+  puts authorization_request.errors.join("\n")
+end
